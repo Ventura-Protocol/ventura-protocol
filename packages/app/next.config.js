a97db3399e413799af88628409f414c83374556f
George <george@abstractpoint.co.uk>
2021-08-04 20:10:34 +0100

    Web3-react added

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    RPC_URL_1: 'https://mainnet.infura.io/v3/84842078b09946638c03157f83405213',
+    RPC_URL_4: 'https://rinkeby.infura.io/v3/84842078b09946638c03157f83405213',
+  }
 }
