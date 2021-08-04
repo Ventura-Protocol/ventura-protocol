b48b176971c0b001805773f3dd31e1c55d918fe1
George <george@abstractpoint.co.uk>
2021-08-04 19:44:11 +0100

    Deploy command with ABI exporting

--- packages/core/pledges/hardhat.config.ts
+++ packages/core/pledges/hardhat.config.ts
@@ -1,6 +1,17 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.7.3",
+ import { HardhatUserConfig } from 'hardhat/types';
+import 'hardhat-deploy';
+
+const config: HardhatUserConfig = {
+  solidity: "0.7.6",
+  defaultNetwork: "hardhat",
+  namedAccounts: {
+    deployer: 0,
+  },
+  paths: {
+    sources: 'src',
+  },
 };
+export default config;
