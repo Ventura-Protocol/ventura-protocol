4ec39e59865a3ea7b4def4a1655963fd3a821954
George <george@abstractpoint.co.uk>
2021-08-10 22:33:44 +0100

    Detail pane retrieving correct ask detail or showing 404

--- packages/app/components/detail-pane.tsx
+++ packages/app/components/detail-pane.tsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from 'react'
 import DefaultErrorPage from 'next/error';
+import { TransactionResponse, TransactionReceipt } from '@ethersproject/providers'
 import { useRouter } from 'next/router'
+import contractsInfo from '../contracts/contractsInfo.json';
+import { useContract } from '../hooks/usecontracts'
+import { stringToDataHexString, dataHexStringToString } from '../utils/bytes';
 
 const DetailPane = () => {
-    const router = useRouter()
+    const [ask, setAsk] = useState(undefined);
+    const router = useRouter();
     console.log(router.query);
+    const { path } = router.query; 
+
+    const contract = useContract(contractsInfo.contracts.Pledges.address, contractsInfo.contracts.Pledges.abi);
+    
+    useEffect(()=>{
+        if (contract && path) {
+            contract?.getAsk(
+                stringToDataHexString(path[0]),
+                path[1],
+            )
+            .then(res => res.totalPledges === 0 ? undefined : res)
+            .then(setAsk)
+        }
+    }, [contract, path]);
+
+    if (ask) {
+        return(<div>Ask: {ask.totalPledges}</div>)
+    }
+
     return(
         <DefaultErrorPage statusCode={404} />
     )
